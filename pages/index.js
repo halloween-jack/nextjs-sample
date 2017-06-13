9782d6e24e348205ff845e5743f8699d63f68379
halloween-jack <halloween.jack.dev@gmail.com>
2017-06-14 08:39:43 +0900

    finish section 02

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,18 @@
+// This is the Link API
+import Link from 'next/link'
+
 const Index = () => (
   <div>
-  <p>Hello Next.js</p>
+    <Link href="/about">
+      <a>About Page</a>
+    </Link>
+    <Link href="/about">
+      <button>Go to About Page Button</button>
+    </Link>
+    <Link href="/about">
+      <div>Go to About Page Div</div>
+    </Link>
+    <p>Hello Next.js</p>
   </div>
 )
 
